27ec52ad4a10f5263a3134fffd4725dbae5146d3
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-04-15 11:32:42 -0500

    Agregando archivos

--- src/main.js
+++ src/main.js
@@ -3,10 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+var firebase = require("firebase/app")
+require("firebase/auth")
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDrxGPFkoAxsVywar2Xt4UIsf5dX0vIgto",
+    authDomain: "crudfirebasevuejs.firebaseapp.com",
+    databaseURL: "https://crudfirebasevuejs.firebaseio.com",
+    projectId: "crudfirebasevuejs",
+    storageBucket: "crudfirebasevuejs.appspot.com",
+    messagingSenderId: "558578021476",
+    appId: "1:558578021476:web:7789913ed8da33150e1b71"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+  	store,
+  	render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router'
+
+var firebase = require("firebase/app")
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		usuario: '',
+		error: ''
+  	},
+  
+	mutations: {
+		setUsuario(state, payload) {
+			state.usuario = payload
+		},
+
+		setError(state, payload) {
+			state.error = payload
+		}
+  	},
+  
+	actions: {
+		crearUsuario({commit}, payload) {
+			firebase.auth().createUserWithEmailAndPassword(payload.email, payload.pass)
+			.then(res => {
+				commit('setUsuario',{email: res.user.email, uid: res.user.uid})
+
+				router.push({name: 'inicio'})
+			})
+			.catch(err => {
+				commit('setError',err.message)
+			})
+		}
+  	},
+	  
+	modules: {
+  	}
 })
