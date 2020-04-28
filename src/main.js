201ec4a8ee4b64bd4e828dfb56edc98d7a5f1cd0
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-04-27 19:43:42 -0500

    Agregando cambios

--- src/main.js
+++ src/main.js
@@ -1,29 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-var firebase = require("firebase/app")
-require("firebase/auth")
+var firebase = require("firebase/app");
+
+require("firebase/auth");
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: "AIzaSyDrxGPFkoAxsVywar2Xt4UIsf5dX0vIgto",
+    apiKey: "AIzaSyDrxGPFkoAxsVywar2Xt4UIsf5dX0vIgto",
     authDomain: "crudfirebasevuejs.firebaseapp.com",
     databaseURL: "https://crudfirebasevuejs.firebaseio.com",
     projectId: "crudfirebasevuejs",
     storageBucket: "crudfirebasevuejs.appspot.com",
     messagingSenderId: "558578021476",
-    appId: "1:558578021476:web:7789913ed8da33150e1b71"
+    appId: "1:558578021476:web:7789913ed8da33150e1b71",
 };
 
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-	router,
-  	store,
-  	render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
